f552e1ce845c636138f3f996248b8c646d3df539
basit1215 <haizabdulbasit225@gmail.com>
2024-03-04 03:14:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -79,6 +79,76 @@ ans=table*multiply;
 document.write(table +" x "+ multiply +" = " + ans + "<br>");
 
 
+document.write("The Temperature Converter:" + "<br>");
+var celsius=27;
+var fahrenheit=75;
+var cal_fahrenheit= (celsius * 9 / 5 ) +32;
+document.write(celsius +"\xB0C is " + cal_fahrenheit + "\xB0F" + "<br>");
 
- 
+var cal_celsius= (fahrenheit - 32)*5/9;
+document.write(fahrenheit + "\xB0F is " + cal_celsius + "\xB0C" + "<br>");
+
+document.write("<b>Shoping Cart</b>" + "<br>");
+var first_item_price=750;
+var first_item_quantity=5;
+var second_item_price=200;
+var second_item_quantity=3;
+var shoping_charges=100;
+var total_price;
+total_price=(first_item_price*first_item_quantity)+(second_item_price*second_item_quantity)+(shoping_charges);
+
+document.write("Price of item 1 is " +  first_item_price + "<br>");
+document.write("Quantity of item 1 is " +  first_item_quantity + "<br>");
+document.write("Price of item 2 is " +   second_item_price + "<br>");
+document.write("Quantity of item 2 is " +   second_item_quantity + "<br>");
+document.write("Shopping Charges "+ shoping_charges  + "<br>" + "<br>");
+document.write("Total cost of your order is "+ total_price + "<br>" + "<br>");
+
+document.write("<b>Marks Sheet</b>" + "<br>");
+var total_marks=1100;
+var obtained_marks=980;
+var percentage;
+percentage=(obtained_marks/total_marks)*100;
+document.write("Total Marks: " +total_marks + "<br>");
+document.write("Marks Obtained: " + obtained_marks + "<br>");
+document.write("Percentage: " + percentage + "%" + "<br>" + "<br>")
+
+document.write("<b>Currency in PKR </b>" + "<br>");
+var dollar=278.33;
+var riyal=74.22;
+var total_amout;
+total_amout=(dollar*10)+(riyal*25);
+document.write("Total Currency in PKR: " + total_amout + "<br>" + "<br>");
+
+var  arithmetic=25;
+arithmetic=arithmetic+5*10/2;
+document.write("Perform all calculations, and answer is: " +arithmetic + "<br>" + "<br>");
+
+document.write("<b>Age Calculator:</b>" + "<br>");
+var current_year=2024;
+var birth_year=2006;
+var myAge;
+myAge=current_year-birth_year;
+ document.write("Current Year: " + current_year + "<br>");
+ document.write("Birth Year: " + birth_year + "<br>");
+ document.write("Your Age is: " + myAge + "<br>" + "<br>");
+
+document.write("<b>Geometrizer:</b>" + "<br>");
+var radius=30;
+var pi=3.142;
+var circum;
+circum=2*radius*pi;
+document.write("Radius of a circle: "+ radius + "<br>");
+document.write("The circumference is: " + circum + "<br>");
+var area;
+area= pi*(radius**2) + "<br>";
+document.write("The area is: " + area + "<br>" + "<br>");
+
+
+
+
+
+
+
+
 
