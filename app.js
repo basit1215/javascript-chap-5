8dabcd6bb4d24406b5100fe3f486e3a627f37301
basit1215 <haizabdulbasit225@gmail.com>
2024-03-03 21:55:51 +0500

    updated

---
+++ app.js
@@ -0,0 +1,84 @@
+var first=3;
+var second=5;
+var third= first+second;
+document.write("Sum of " + first + " and " + second + " is " + third + "<br>");
+var third=first-second;
+document.write("subtraction of " + first + " and " + second + " is " + third + "<br>");
+var third=first*second;
+document.write("multiplication of "+ first + " and " + second + " is " + third + "<br>");
+var third=first/second;
+document.write("divison of " + first + " and " + second + " is " + third + "<br>");
+var third=first%second;
+document.write("modulus of " + first + " and " + second + " is " + third + "<br>" + "<br>");
+
+var num;
+document.write("Value after variable declaration is:" + num + "<br>");
+
+var number=20;
+document.write("Initial value is:" + number + "<br>");
+
+number++;
+document.write("Value after increment is:" + number + "<br>");
+
+number=number+12;
+document.write("Value after addition is: "+ number + "<br>");
+
+number--;
+document.write("Value after decrement is:" + number + "<br>");
+
+number=number%3;
+document.write("Value after modulus by 3 a reminder is:" + number + "<br>" + "<br>");
+
+ var ticket_price=600;
+ var total_ticket=5;
+ total=total_ticket*ticket_price;
+ document.write("Total cost to buy " + total_ticket + " tickets to a movie is  <b>" +  total  + "PKR</b>" + "<br>" + "<br>")
+
+
+ document.write("Table of 4" + "<br>");
+
+var table=8;
+var multiply=1;
+var ans=table*multiply;
+document.write(table +" x "+ multiply +" = " + ans + "<br>");
+
+multiply++;
+ans=table*multiply;
+document.write(table +" x "+ multiply +" = " + ans + "<br>");
+
+multiply++;
+ans=table*multiply;
+document.write(table +" x "+ multiply +" = " + ans + "<br>");
+
+multiply++;
+ans=table*multiply;
+document.write(table +" x "+ multiply +" = " + ans + "<br>");
+
+multiply++;
+ans=table*multiply;
+document.write(table +" x "+ multiply +" = " + ans + "<br>");
+
+multiply++;
+ans=table*multiply;
+document.write(table +" x "+ multiply +" = " + ans + "<br>");
+
+multiply++;
+ans=table*multiply;
+document.write(table +" x "+ multiply +" = " + ans + "<br>");
+
+multiply++;
+ans=table*multiply;
+document.write(table +" x "+ multiply +" = " + ans + "<br>");
+
+multiply++;
+ans=table*multiply;
+document.write(table +" x "+ multiply +" = " + ans + "<br>");
+
+multiply++;
+ans=table*multiply;
+document.write(table +" x "+ multiply +" = " + ans + "<br>");
+
+
+
+ 
+
